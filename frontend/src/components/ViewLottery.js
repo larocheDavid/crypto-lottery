628391643528eca849ed2a397ed466da48a9131e
david.laroche <david.laroche@etu.hesge.ch>
2022-03-08 14:07:55 +0100

    style

--- frontend/src/components/ViewLottery.js
+++ frontend/src/components/ViewLottery.js
@@ -1,42 +1,76 @@
-import React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
 
 import {
-    TextField,
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle
-} from '@material-ui/core';
+  TextField,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@material-ui/core";
 
 //const ViewLottery = ({name, ticketPrice, duration}) => {
 const ViewLottery = (props) => {
+  const name = props.name;
+  const ticketPrice = props.ticketPrice;
+  const duration = props.duration;
 
-    const name = props.name;
-    const ticketPrice = props.ticketPrice;
-    const duration = props.duration;
+  const [open, setOpen] = React.useState(false);
 
-    const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClickOpen = () => {
-          setOpen(true);
-    };
-      
-    const handleClose = () => {
-          setOpen(false);
-    };
-    
-    const columns = [
-        { field: 'name_', headerName: 'Name', width: 70 },
-        { field: 'ticketPrice_', headerName: 'Ticket Price', width: 130 },
-        { field: 'duration_', headerName: 'Duration', width: 130 },
-      ];
-    
-    const rows = [
-        { name_: name, ticketPrice_: ticketPrice, duration_: duration},
-    ];
-/*
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const columns = [
+    { field: "name_", headerName: "Name", width: 70 },
+    { field: "ticketPrice_", headerName: "Ticket Price", width: 130 },
+    { field: "duration_", headerName: "Duration", width: 130 },
+  ];
+
+  const rows = [
+    { name_: name, ticketPrice_: ticketPrice, duration_: duration },
+  ];
+
+  return (
+    <>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        View Lottery
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">View Lottery</DialogTitle>
+        <DialogContent>
+          <div style={{ height: 400, width: "100%" }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+              checkboxSelection
+            />
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default ViewLottery;
+
+ /*
     const renderForm = () => {
         return (
             
@@ -48,26 +82,17 @@ const ViewLottery = (props) => {
             </div>
             
         );
-    }*/
-    
+    }
+    export default function DataTable() {
   return (
-    <>
-          <Button variant="outlined" color="primary" onClick={handleClickOpen} >
-              View Lottery
-          </Button>
-          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-          <DialogTitle id="form-dialog-title">View Lottery</DialogTitle>
-              <DialogContent>
-              ...
-              </DialogContent>
-              <DialogActions>
-                  <Button onClick={handleClose} color="primary">
-                      Close
-                  </Button>
-              </DialogActions>
-          </Dialog>
-      </>
-  )
-}
-
-export default ViewLottery
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
+  );
+}*/
